502f5e15c84772c25e5157203d8ae905a534128b
Barbara Se <seidinger-barbara@gmx.de>
2022-01-29 14:31:20 +0100

    refactored, formatted, location and speech works

--- Android App/AndroidApp/app/src/main/java/com/example/coachapp/SpeechRecognizerSetup.java
+++ Android App/AndroidApp/app/src/main/java/com/example/coachapp/SpeechRecognizerSetup.java
@@ -2,34 +2,37 @@ package com.example.coachapp;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.Locale;
 
 public class SpeechRecognizerSetup {
 
-    public static final Integer RecordAudioRequestCode = 1;
-    public static SpeechRecognizer speechRecognizer;
-    public static Intent speechRecognizerIntent;
-    private static Context context;
+    public final Integer RecordAudioRequestCode = 1;
+    public SpeechRecognizer speechRecognizer;
+    public Intent speechRecognizerIntent;
+    private Activity activity;
 
-    public SpeechRecognizerSetup(Context context) {
-        this.context = context;
-        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(context);
+    public SpeechRecognizerSetup(Activity activity) {
+        this.activity = activity;
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(activity);
         speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
     }
 
-    public static void checkPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.RECORD_AUDIO}, RecordAudioRequestCode);
+    public void checkPermission() {
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.RECORD_AUDIO}, RecordAudioRequestCode);
+            }
         }
     }
 }
--- Android App/AndroidApp/app/src/main/java/com/example/coachapp/view/MainActivity.java
+++ Android App/AndroidApp/app/src/main/java/com/example/coachapp/view/MainActivity.java
@@ -1,15 +1,7 @@
 package com.example.coachapp.view;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Looper;
-import android.provider.Settings;
 import android.speech.RecognitionListener;
 import android.speech.SpeechRecognizer;
 import android.util.Log;
@@ -20,19 +12,11 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.example.coachapp.R;
 import com.example.coachapp.SpeechRecognizerSetup;
 import com.example.coachapp.connection.RetrofitInstance;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.example.coachapp.gps.GPSLocation;
 
 import java.util.ArrayList;
 
@@ -43,15 +27,14 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "RecognitionListener";
+    private GPSLocation gpsLocation;
+    private SpeechRecognizerSetup speechRecognizerSetup;
 
     private ImageButton voiceButton;
     private TextView speechToTextView;
     private TextView textToSpeechView;
 
     private String spokenText;
-
-    private FusedLocationProviderClient mFusedLocationClient;
-    int PERMISSION_ID = 44;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,95 +43,30 @@ public class MainActivity extends AppCompatActivity {
         speechToTextView = findViewById(R.id.speechToText);
         textToSpeechView = findViewById(R.id.textToSpeech);
         voiceButton = findViewById(R.id.voiceButton);
-        new SpeechRecognizerSetup(this);
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        speechRecognizerSetup = new SpeechRecognizerSetup(this);
+        gpsLocation = new GPSLocation(this);
 
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            SpeechRecognizerSetup.checkPermission();
-        }
         recognizeSpeech();
 
         voiceButton.setOnClickListener(view -> {
             speechToTextView.setVisibility(View.VISIBLE);
             voiceButton.setImageResource(R.mipmap.voice_recorder);
             new RetrofitInstance();
-            SpeechRecognizerSetup.speechRecognizer.startListening(SpeechRecognizerSetup.speechRecognizerIntent);
+            speechRecognizerSetup.speechRecognizer.startListening(speechRecognizerSetup.speechRecognizerIntent);
         });
 
         findViewById(R.id.sendBtn).setOnClickListener(view -> {
             System.out.println("PRessed location");
-            getLastLocation();
+            gpsLocation.getLastLocation();
         });
-    }
-
-
-    @SuppressLint("MissingPermission")
-    private void getLastLocation() {
-        if (checkPermissions()) {
-            if (isLocationEnabled()) {
-                mFusedLocationClient.getLastLocation().addOnCompleteListener(task -> {
-                    Location location = task.getResult();
-                    if (location == null) {
-                        requestNewLocationData();
-                    } else {
-                        System.out.println("96: long " + location.getLongitude() + " lat " + location.getLatitude());
-                    }
-                });
-            } else {
-                Toast.makeText(this, "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        } else {
-            requestPermissions();
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private void requestNewLocationData() {
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(5);
-        mLocationRequest.setFastestInterval(0);
-        mLocationRequest.setNumUpdates(1);
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
-    }
-
-    private LocationCallback mLocationCallback = new LocationCallback() {
-
-        @Override
-        public void onLocationResult(LocationResult locationResult) {
-            Location mLastLocation = locationResult.getLastLocation();
-            System.out.println("134: long " + mLastLocation.getLongitude() + " lat " + mLastLocation.getLatitude());
-        }
-    };
-
-    private boolean checkPermissions() {
-        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-    }
-
-    // method to request for permissions
-    private void requestPermissions() {
-        ActivityCompat.requestPermissions(this, new String[]{
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ID);
-    }
-
-    private boolean isLocationEnabled() {
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == PERMISSION_ID) {
+        if (requestCode == gpsLocation.getPERMISSION_ID()) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLastLocation();
+                gpsLocation.getLastLocation();
             }
         }
     }
@@ -156,13 +74,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
-        if (checkPermissions()) {
-            getLastLocation();
+        if (gpsLocation.checkPermissions()) {
+            gpsLocation.getLastLocation();
         }
     }
 
     private void recognizeSpeech() {
-        SpeechRecognizerSetup.speechRecognizer.setRecognitionListener(new RecognitionListener() {
+        speechRecognizerSetup.checkPermission();
+        speechRecognizerSetup.speechRecognizer.setRecognitionListener(new RecognitionListener() {
             @Override
             public void onReadyForSpeech(Bundle bundle) {
                 Log.d(TAG, "onReadyForSpeech");
