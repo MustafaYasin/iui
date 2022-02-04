7dd88b7ac34454883ff18fcd4cdaf561b640002f
richardhofmann2711 <r.hofmann2711@gmail.com>
2022-02-04 20:27:12 +0100

    added 12 dummypersons as dict

---
+++ Backend/database/dummypersons.py
@@ -0,0 +1,118 @@
+person_1 = {
+    "id":1,
+    "workouts": 3,
+    "level": "beginner",
+    "age": 25,
+    "sex": "male",
+    "goal": "gain muscle",
+    "trainingplan": "tp_1"
+}
+
+person_2 = {
+    "id":2,
+    "workouts": 4,
+    "level": "beginner",
+    "age": 30,
+    "sex": "male",
+    "goal": "gain muscle",
+    "trainingplan": "tp_2"
+}
+
+person_3 = {
+    "id":3,
+    "workouts": 4,
+    "level": "advanced",
+    "age": 18,
+    "sex": "male",
+    "goal": "gain muscle",
+    "trainingplan": "tp_3"
+}
+
+person_4 = {
+    "id":4,
+    "workouts": 5,
+    "level": "beginner",
+    "age": 28,
+    "sex": "male",
+    "goal": "gain muscle",
+    "trainingplan": "tp_4"
+}
+
+person_5 = {
+    "id":5,
+    "workouts": 5,
+    "level": "advanced",
+    "age": 25,
+    "sex": "male",
+    "goal": "gain muscle",
+    "trainingplan": "tp_5"
+}
+
+person_6 = {
+    "id":6,
+    "workouts": 6,
+    "level": "advanced",
+    "age": 23,
+    "sex": "male",
+    "goal": "gain muscle",
+    "trainingplan": "tp_6"
+}
+person_7 = {
+    "id":7,
+    "workouts": 3,
+    "level": "beginner",
+    "age": 38,
+    "sex": "male",
+    "goal": "gain muscle",
+    "trainingplan": "tp_1"
+}
+
+person_8 = {
+    "id":8,
+    "workouts": 4,
+    "level": "beginner",
+    "age": 20,
+    "sex": "male",
+    "goal": "gain muscle",
+    "trainingplan": "tp_2"
+}
+
+person_9 = {
+    "id":9,
+    "workouts": 4,
+    "level": "advanced",
+    "age": 35,
+    "sex": "male",
+    "goal": "gain muscle",
+    "trainingplan": "tp_3"
+}
+
+person_10 = {
+    "id":10,
+    "workouts": 5,
+    "level": "beginner",
+    "age": 43,
+    "sex": "male",
+    "goal": "gain muscle",
+    "trainingplan": "tp_4"
+}
+
+person_11 = {
+    "id":11,
+    "workouts": 5,
+    "level": "advanced",
+    "age": 45,
+    "sex": "male",
+    "goal": "gain muscle",
+    "trainingplan": "tp_5"
+}
+
+person_12 = {
+    "id":12,
+    "workouts": 6,
+    "level": "advanced",
+    "age": 43,
+    "sex": "male",
+    "goal": "gain muscle",
+    "trainingplan": "tp_6"
+}
