bec3c01094d93efe97358206e474b3636420396d
richardhofmann2711 <r.hofmann2711@gmail.com>
2022-02-04 20:14:31 +0100

    added dict with trainingsplans

---
+++ Backend/database/trainingsplan.py
@@ -0,0 +1,263 @@
+trainingplan_1 = {
+  "title":"tp_1",
+  "trainings": 3,
+  "level": "all",
+  "split": 0,
+  "sets": 3,
+  "day_1":
+    {
+        "area": "full body",
+        "exercises":1
+      },
+  "day_2": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+  "day_3": 
+    {
+        "area": "full body",
+        "exercises":1
+      },
+  "day_4": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+  "day_5": 
+    {
+        "area": "full body",
+        "exercises":1
+      },
+  "day_6": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+  "day_7": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+}
+
+trainingplan_2 = {
+  "title":"tp_2",
+  "trainings": 4,
+  "level": "Beginner",
+  "split": 2,
+  "sets": 3,
+  "day_1":
+    {
+        "area": "upper body",
+        "exercises":2
+      },
+  "day_2": 
+    {
+        "area": "lower body",
+        "exercises":2
+      },
+  "day_3": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+  "day_4": 
+    {
+        "area": "upper body",
+        "exercises":2
+      },
+  "day_5": 
+    {
+        "area": "lower body",
+        "exercises":2
+      },
+  "day_6": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+  "day_7": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+}
+
+
+trainingplan_3 = {
+  "title":"tp_3",
+  "trainings": 4,
+  "level": "advanced",
+  "split": 4,
+  "sets": 4,
+  "day_1":
+    {
+        "area": "back, biceps",
+        "exercises":3
+      },
+  "day_2": 
+    {
+        "area": "chest, triceps",
+        "exercises":3
+      },
+  "day_3": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+  "day_4": 
+    {
+        "area": "legs",
+        "exercises":2
+      },
+  "day_5": 
+    {
+        "area": "shoulder",
+        "exercises":4
+      },
+  "day_6": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+  "day_7": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+}
+
+
+
+trainingplan_4 = {
+  "title":"tp_4",
+  "trainings": 5,
+  "level": "Beginner",
+  "split": 2,
+  "sets": 3,
+  "day_1":
+    {
+        "area": "upper body",
+        "exercises":2
+      },
+  "day_2": 
+    {
+        "area": "lower body",
+        "exercises":2
+      },
+  "day_3": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+  "day_4": 
+    {
+        "area": "upper body",
+        "exercises":2
+      },
+  "day_5": 
+    {
+        "area": "lower body",
+        "exercises":2
+      },
+  "day_6": 
+    {
+        "area": "cardio",
+        "exercises":1
+      },
+  "day_7": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+}
+
+
+trainingplan_5 = {
+  "title":"tp_5",
+  "trainings": 5,
+  "level": "advanced",
+  "split": 5,
+  "sets": 4,
+  "day_1":
+    {
+        "area": "chest",
+        "exercises":4
+      },
+  "day_2": 
+    {
+        "area": "back",
+        "exercises":5
+      },
+  "day_3": 
+    {
+        "area": "schoulder",
+        "exercises":4
+      },
+  "day_4": 
+    {
+        "area": "legs",
+        "exercises":5
+      },
+  "day_5": 
+    {
+        "area": "biceps, triceps",
+        "exercises":3
+      },
+  "day_6": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+  "day_7": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+}
+
+
+
+trainingplan_6 = {
+  "title":"tp_6",
+  "trainings": 6,
+  "level": "advanced",
+  "split": 3,
+  "sets": 4,
+  "day_1":
+    {
+        "area": "chest, shoulder, triceps",
+        "exercises":3
+      },
+  "day_2": 
+    {
+        "area": "back, biceps",
+        "exercises":3
+      },
+  "day_3": 
+    {
+        "area": "legs",
+        "exercises":4
+      },
+  "day_4": 
+    {
+        "area": "chest, shoulder, triceps",
+        "exercises":3
+      },
+  "day_5": 
+    {
+        "area": "back, biceps",
+        "exercises":3
+      },
+  "day_6": 
+    {
+        "area": "legs",
+        "exercises":4
+      },
+  "day_7": 
+    {
+        "area": "rest",
+        "exercises":0
+      },
+}
