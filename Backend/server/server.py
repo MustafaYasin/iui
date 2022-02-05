4acfc19aaccd2f9fd2e8cecf740217e8e7eab5dc
Sandra Wackerl <Sandra.Wackerl@campus.lmu.de>
2022-02-05 15:48:43 +0100

    changes in backend (server)

--- Backend/server/server.py
+++ Backend/server/server.py
@@ -1,23 +1,38 @@
 from http.server import BaseHTTPRequestHandler
 from routes import routes
+import json
 
 class Server(BaseHTTPRequestHandler):
   def do_HEAD(self):
     return
     
   def do_GET(self):
-    self.respond()
+    # self.respond()
+    # if self.path == "/spokenText":
+    #     self.send_response(200)
+    #     self.end_headers()
+    #     self.wfile.write(b'Test!')
+    return
     
   def do_POST(self):
+    if self.path == "/spokenText":
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Blubberdiblubb?')
+
+    # if self.path == "/user":
+        # json.dumps(newUser1 = {
+        #     "name": ""
+        # })
+
     return
     
   def handle_http(self, status, content_type):
     self.send_response(status)
     self.send_header('Content-type', content_type)
     self.end_headers()
-    
-    # if self.path == "/spokenText":
-        # self.respond("Blabalbala")
+
+
     route_content = routes[self.path]
     return bytes(route_content, "UTF-8")
     
