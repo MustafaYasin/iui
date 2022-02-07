215ba44316411ed40cc3e1ffe432d07ffd949b5c
richardhofmann2711 <r.hofmann2711@gmail.com>
2022-02-07 15:07:23 +0100

    combined init_db files to one

--- Backend/database/init_tp_and_user_DB.py
+++ Backend/database/init_tp_and_user_DB.py
@@ -1,15 +1,23 @@
 from pymongo import MongoClient
 from trainingsplan import *
+from dummypersons import *
+
 
 # Create connection to MongoDB
 client = MongoClient("mongodb+srv://mustafayasin:nisani2404@cluster0.oxj2y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client['iui']
-collection = db['user']
+
+# push data to database user
+users = db['user']
+persons= [person_1, person_2, person_3, person_4, person_5, person_6, person_7, person_8, person_9, person_10, person_11, person_12]
+
+for person in persons:
+    users.insert_one(person)
 
 
 # push data to database trainingsprogramm
-collection = db['trainingprogramm']
+trainingplans = db['trainingprogramm']
 trainingsprogramms = [trainingplan_1, trainingplan_2, trainingplan_3, trainingplan_4, trainingplan_5, trainingplan_6]
 
 for trainingsprogramm in trainingsprogramms:
-    collection.insert_one(trainingsprogramm)
+    trainingplans.insert_one(trainingsprogramm)
